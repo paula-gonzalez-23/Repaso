d57b2e8388cad74dbf7ac358667a4c4429c27f62
Paula Gonzalez <paulav.gonzalez@urosario.edu.co>
2024-02-11 14:15:46 -0500

    Edited Cara y Sello
    Incorpore el ciclo for a 5 partidas, muestra por pantalla la cantidad de partidas ganadas y perdidas

--- Caraysello.cpp
+++ Caraysello.cpp
@@ -7,42 +7,56 @@ int main(){
 
     int aleatorio = 0;
     int SeleccionUsuario = 0;
-
-    cout<<"Juego de Cara y Sello"<<endl;
-    cout<<"1. Cara"<<endl;
-    cout<<"2. Sello"<<endl;
-
-
-    cout<<"Ingrese la opción con la quiere jugar: "<<endl;
-    cin>>SeleccionUsuario;
+    int Partidasganadas = 0;
+    int Partidasperdidas = 0;
 
     srand(time(NULL));
-    aleatorio = 1+(rand()%(3-1));
 
-    switch (aleatorio){
-    case 1:
-        if (SeleccionUsuario == 1) {
-            cout << "El aleatorio es Cara" << endl;
-            cout << "¡Felicitaciones! Ha ganado" << endl;
-        } else {
-            cout << "El aleatorio es Cara" << endl;
-            cout << ":( Sigue Intentando, esta no fue tu mejor elección" << endl;
-        }
-        break;
+    for (int partida = 1; partida <= 5; ++partida){
+        cout<<"Partida # "<<partida<<endl;
 
-    case 2:
-        if (SeleccionUsuario == 2) {
-            cout << "El aleatorio es Sello" << endl;
-            cout << "¡Felicitaciones! Ha ganado" << endl;
-        } else {
-            cout << "El aleatorio es Sello" << endl;
-            cout << ":( Sigue Intentando, esta no fue tu mejor elección" << endl;
-        }
-        break;
+        cout<<"Juego de Cara y Sello"<<endl;
+        cout<<"1. Cara"<<endl;
+        cout<<"2. Sello"<<endl;
+
+
+        cout<<"Ingrese la opción con la quiere jugar: "<<endl;
+        cin>>SeleccionUsuario;
+
         
-    default:
-        break;
+        aleatorio = 1+(rand()%(3-1));
+
+        switch (aleatorio){
+        case 1:
+            if (SeleccionUsuario == 1) {
+                cout << "El aleatorio es Cara" << endl;
+                cout << "¡Felicitaciones! Ha ganado" << endl;
+                ++Partidasganadas;
+            } else {
+                cout << "El aleatorio es Cara" << endl;
+                cout << ":( Sigue Intentando, esta no fue tu mejor elección" << endl;
+                ++Partidasperdidas;
+            }
+            break;
+
+        case 2:
+            if (SeleccionUsuario == 2) {
+                cout << "El aleatorio es Sello" << endl;
+                cout << "¡Felicitaciones! Ha ganado" << endl;
+                ++Partidasganadas;
+            } else {
+                cout << "El aleatorio es Sello" << endl;
+                cout << ":( Sigue Intentando, esta no fue tu mejor elección" << endl;
+                ++Partidasperdidas;
+            }
+            break;
+            
+        default:
+            break;
+        }
     }
+
+    cout<<"Juego terminado. Partidas ganadas: "<<Partidasganadas<<", Partidas perdidas: "<<Partidasperdidas<<endl;
 
     return 0;
 }
