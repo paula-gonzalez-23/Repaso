ba22564d832bff15ff1645807716cd39663cfdf5
Paula Gonzalez <paulav.gonzalez@urosario.edu.co>
2024-02-11 11:23:07 -0500

    Edited Calculadora
    Agregue do while al programa y que el usuario decida si quiere salirse o seguir realizando operaciones

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -13,8 +13,9 @@ int main(){
     int division = 0;
     int cuadrado1 = 0;
     int cuadrado2 = 0;
-    int raiz1 = 0;
-    int raiz2 = 0;
+    double raiz1 = 0;
+    double raiz2 = 0;
+    string respuesta;
 
     cout<<"Bienvenido a la Calculadora: "<<endl;
     cout<<"1. Suma"<<endl;
@@ -24,61 +25,62 @@ int main(){
     cout<<"5. Cuadrado"<<endl;
     cout<<"6. Raiz Cuadrada"<<endl;
 
-    cout<<"Ingrese la opción que desea realizar: "<<endl;
-    cin>>opcion;
+    do {
 
-    cout<<"Ingrese el primer número: "<<endl;
-    cin>>num1;
+        cout<<"Ingrese la opción que desea realizar: "<<endl;
+        cin>>opcion;
 
-    cout<<"Ingrese el segundo número: "<<endl;
-    cin>>num2;
+        cout<<"Ingrese el primer número: "<<endl;
+        cin>>num1;
 
-    switch(opcion){
-    case 1:
-        suma = num1 + num2;
-        cout<<"El resultado de su suma es: "<<suma<<endl;
+        cout<<"Ingrese el segundo número: "<<endl;
+        cin>>num2;
 
-        break;
+        
+        switch(opcion){
+            case 1:
+                suma = num1 + num2;
+                cout<<"El resultado de su suma es: "<<suma<<endl;
+                break;
 
-    case 2:
-        resta = num1 - num2;
-        cout<<"El resultado de su resta es: "<<resta<<endl;
+            case 2:
+                resta = num1 - num2;
+                cout<<"El resultado de su resta es: "<<resta<<endl;
+                break;
 
-        break;
+            case 3:
+                multiplicacion = num1*num2;
+                cout<<"El resultado de su multiplicación es: "<<multiplicacion<<endl;
+                break;
 
-    case 3:
-        multiplicacion = num1*num2;
-        cout<<"El resultado de su multiplicación es: "<<multiplicacion<<endl;
+            case 4:
+                division = num1/num2;
+                cout<<"El resultado de su división es: "<<division<<endl;
+                break;
+            
+            case 5: 
+                cuadrado1 = num1*num1;
+                cuadrado2 = num2*num2;
+                cout<<"El resultado del cuadrado del número uno es: "<<cuadrado1<<endl;
+                cout<<"El resultado del cuadrado del número dos es: "<<cuadrado2<<endl;
+                break;
+            
+            case 6:
+                raiz1 = sqrt(num1);
+                raiz2 = sqrt(num2);
+                cout<<"El resultado de la raiz del número uno es: "<<raiz1<<endl;
+                cout<<"El resultado de la raiz del número dos es: "<<raiz2<<endl;
+                break;
 
-        break;
+            default:
+                break;
 
-    case 4:
-        division = num1/num2;
-        cout<<"El resultado de su división es: "<<division<<endl;
+        }
 
-        break;
-    
-    case 5: 
-        cuadrado1 = num1*num1;
-        cuadrado2 = num2*num2;
-        cout<<"El resultado del cuadrado del número uno es: "<<cuadrado1<<endl;
-        cout<<"El resultado del cuadrado del número dos es: "<<cuadrado2<<endl;
+        cout<<"Desea realizar mas operaciones? (si/no): "<<endl;
+        cin>>respuesta;
 
-        break;
-    
-    case 6:
-        raiz1 = sqrt(num1);
-        raiz2 = sqrt(num2);
-
-        cout<<"El resultado de la raiz del número uno es: "<<raiz1<<endl;
-        cout<<"El resultado de la raiz del número dos es: "<<raiz2<<endl;
-
-        break;
-
-    default:
-        break;
-    }
-
+    } while (respuesta == "si" || respuesta == "SI");
 
     return 0;
 }
