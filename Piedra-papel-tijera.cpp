745f3fee9b223df5dda9d94d1bdafcf101655fbb
Paula Gonzalez <paulav.gonzalez@urosario.edu.co>
2024-02-11 14:03:12 -0500

    Edited Piedra, Papel, Tijera
    Incorpore el ciclo for a 5 partidas, se muestra por pantalla cantidad de partidas ganadas, perdidas y empatadas

--- Piedra-papel-tijera.cpp
+++ Piedra-papel-tijera.cpp
@@ -7,59 +7,81 @@ int main(){
 
     int SeleccionUsuario = 0;
     int SeleccionComputadora;
+    int Partidasganadas = 0;
+    int Partidasperdidas = 0;
+    int Empate = 0;
 
-    cout<<"Juego Piedra Papel o Tijera"<<endl;
-    cout<<"1. Piedra"<<endl;
-    cout<<"2. Papel"<<endl;
-    cout<<"3. Tijera"<<endl;
-
-    cout<<"Ingrese la opción con la quiere jugar: "<<endl;
-    cin>>SeleccionUsuario;
-
-    srand(time(NULL));
-    SeleccionComputadora = 1+(rand()%(4-1));
-
-    switch (SeleccionComputadora){
-    case 1:
-        if (SeleccionUsuario == 1){
-            cout<<"Empate, ambos pensaron igual y sacaron Piedra :/ "<<endl;
-        }else if (SeleccionUsuario == 2) {
-            cout<<"La computadora eligio Piedra"<<endl;
-            cout<<"Ganaste, fuiste más inteligente que la computadora :) "<<endl;
-        }else if (SeleccionUsuario == 3){
-            cout<<"La computadora eligio Piedra"<<endl;
-            cout<<"Perdiste, analiza más y no seas tan apresurado :( "<<endl;
-        }
-        break;
     
-    case 2:
-        if (SeleccionUsuario == 1){
-            cout<<"La computadora eligio Papel"<<endl;
-            cout<<"Perdiste, analiza más y no seas tan apresurado :( "<<endl;
-        }else if (SeleccionUsuario == 2) {
-            cout<<"Empate, ambos pensaron igual y sacaron Papel :/ "<<endl;
-        }else if (SeleccionUsuario == 3){
-            cout<<"La computadora eligio Papel"<<endl;
-            cout<<"Ganaste, fuiste más inteligente que la computadora :) "<<endl;
-            
-        }
-        break;
+    srand(time(NULL));
+    
 
-     case 3:
-        if (SeleccionUsuario == 1){
-            cout<<"La computadora eligio Tijera"<<endl;
-            cout<<"Ganaste, fuiste más inteligente que la computadora :)"<<endl;
-        }else if (SeleccionUsuario == 2) {
-            cout<<"La computadora eligio Tijera"<<endl;
-            cout<<"Perdiste, analiza más y no seas tan apresurado :( "<<endl;
-        }else if (SeleccionUsuario == 3){
-            cout<<"Empate, ambos pensaron igual y sacaron Papel :/ "<<endl;
-        }
-        break;
+    for (int partida = 1; partida <= 5; ++partida){
+        cout<<"Partida # "<<partida<<endl;
 
-    default:
-        break;
+        cout<<"Juego Piedra Papel o Tijera"<<endl;
+        cout<<"1. Piedra"<<endl;
+        cout<<"2. Papel"<<endl;
+        cout<<"3. Tijera"<<endl;
+
+        cout<<"Ingrese la opción con la quiere jugar: "<<endl;
+        cin>>SeleccionUsuario;
+
+        SeleccionComputadora = 1+(rand()%(4-1));
+
+
+        switch (SeleccionComputadora){
+        case 1:
+            if (SeleccionUsuario == 1){
+                cout<<"Empate, ambos pensaron igual y sacaron Piedra :/ "<<endl;
+                ++Empate;
+            }else if (SeleccionUsuario == 2) {
+                cout<<"La computadora eligio Piedra"<<endl;
+                cout<<"Ganaste, fuiste más inteligente que la computadora :) "<<endl;
+                ++Partidasganadas;
+            }else if (SeleccionUsuario == 3){
+                cout<<"La computadora eligio Piedra"<<endl;
+                cout<<"Perdiste, analiza más y no seas tan apresurado :( "<<endl;
+                ++Partidasperdidas;
+            }
+            break;
+        
+        case 2:
+            if (SeleccionUsuario == 1){
+                cout<<"La computadora eligio Papel"<<endl;
+                cout<<"Perdiste, analiza más y no seas tan apresurado :( "<<endl;
+                ++Partidasperdidas;
+            }else if (SeleccionUsuario == 2) {
+                cout<<"Empate, ambos pensaron igual y sacaron Papel :/ "<<endl;
+                ++Empate;
+            }else if (SeleccionUsuario == 3){
+                cout<<"La computadora eligio Papel"<<endl;
+                cout<<"Ganaste, fuiste más inteligente que la computadora :) "<<endl;
+                ++Partidasganadas;
+                
+            }
+            break;
+
+        case 3:
+            if (SeleccionUsuario == 1){
+                cout<<"La computadora eligio Tijera"<<endl;
+                cout<<"Ganaste, fuiste más inteligente que la computadora :)"<<endl;
+                ++Partidasganadas;
+            }else if (SeleccionUsuario == 2) {
+                cout<<"La computadora eligio Tijera"<<endl;
+                cout<<"Perdiste, analiza más y no seas tan apresurado :( "<<endl;
+                ++Partidasperdidas;
+            }else if (SeleccionUsuario == 3){
+                cout<<"Empate, ambos pensaron igual y sacaron Papel :/ "<<endl;
+                ++Empate;
+            }
+            break;
+
+        default:
+            break;
+        }
     }
+
+    cout<<"Juego terminado. Partidas ganadas: "<<Partidasganadas<<", Partidas perdidas: "<<Partidasperdidas<<", Empates: "<<Empate<<endl;
 
     return 0;
 
