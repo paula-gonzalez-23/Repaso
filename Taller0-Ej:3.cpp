2f2c6079a22d936f831d84a386d738de84f162bd
Paula Gonzalez <paulav.gonzalez@urosario.edu.co>
2024-02-12 11:08:09 -0500

    Create Third Exercise of the workshop 0
    Elabore el programa que le aumenta el sueldo a un trabajador si es menor a un millon de pesos

---
+++ Taller0-Ej:3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    float sueldo = 0;
+    float Aumento = 0;
+    float Nuevosueldo = 0;
+
+
+    cout<<"Ingrese el valor de su sueldo actual: "<<endl;
+    cin>>sueldo;
+
+    if (sueldo < 1000000){
+        Aumento = sueldo*0.15;
+        Nuevosueldo = sueldo + Aumento;
+        cout<<"Su nuevo sueldo es: "<<Nuevosueldo<<endl;
+    }else {
+        cout<<"Su sueldo es mayor a 1000000, no tiene aumento :("<<endl;
+    }
+
+    return 0;
+}
