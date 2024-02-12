956ecb2a6aeaae54a477f6f993b5eb3b85901542
Paula Gonzalez <paulav.gonzalez@urosario.edu.co>
2024-02-12 11:48:45 -0500

    Create Fifth Exercise of the workshop 0
    Elabore un programa con while que haga  la suma de los numeros del 1 al 100

---
+++ Taller0-Ej:6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int numero = 1;
+    int suma = 0;
+
+    while (numero <= 100){
+        suma += numero;
+        numero++;
+    }
+
+    cout<<"La suma de los numeros del 1 al 100 es: "<<suma<<endl;
+
+    return 0;
+}
