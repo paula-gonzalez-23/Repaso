1b76edd8280022d21464f18836d6581e96541c93
Paula Gonzalez <paulav.gonzalez@urosario.edu.co>
2024-02-12 12:04:49 -0500

    Edited First Exercise
    Separar para mejor visualizacion

--- Taller0-Ej:1.cpp
+++ Taller0-Ej:1.cpp
@@ -16,14 +16,17 @@ int main(){
         Descuento = ImpuestoPredial*0.2;
         Total = ImpuestoPredial-Descuento;
         cout<<"Su valor a pagar con descuento es: "<<Total<<endl;
+
     }else if (ImpuestoPredial > 150000 && ImpuestoPredial <= 300000){
         Descuento = ImpuestoPredial*0.3;
         Total = ImpuestoPredial-Descuento;
         cout<<"Su valor a pagar con descuento es: "<<Total<<endl;
+
     }else if (ImpuestoPredial > 300000 && ImpuestoPredial <= 800000){
         Descuento = ImpuestoPredial*0.4;
         Total = ImpuestoPredial-Descuento;
         cout<<"Su valor a pagar con descuento es: "<<Total<<endl;
+        
     }else if (ImpuestoPredial > 800000){
         Descuento = ImpuestoPredial*0.5;
         Total = ImpuestoPredial-Descuento;
